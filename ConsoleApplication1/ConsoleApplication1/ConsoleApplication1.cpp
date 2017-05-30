50385cbcda2d102a9f78a03cbb05a2f5d8083d66
Dethlord <Dethlord@rambler.ru>
2017-05-30 22:50:08 +1000

    Добавил новый функционал

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	int kkk(1);
+	cin >> kkk;
+    return 0;
+}
+
